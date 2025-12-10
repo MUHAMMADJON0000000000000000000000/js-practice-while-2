023a42a17cd1a36df4d7052d3b1c49eb15acffef
Muhammadjon coding <86595049+MUHAMMADJON0000000000000000000000@users.noreply.github.com>
2025-12-10 13:44:17 +0500

    sub

---
+++ index.js
@@ -0,0 +1,133 @@
+//itger 12
+
+// let a = 431;
+// let b = parseInt(a / 10);
+// let d = b % 10; //3
+// let s = parseInt(b / 10); //4
+// let c = a % 10; //1
+// console.log(c * 100 + d * 10 + s);
+
+//integer 13
+// let a = 713; //137
+// let b = parseInt(a / 10); //71
+// let f = parseInt(b / 10); //7
+// let q = parseInt(a % 100);
+// let v = q % 10; //3
+// let c = b % 10; //1
+// console.log(c * 100 + v * 10 + f);
+
+// intiger 14
+// let a = 875; //578
+// let b = 100;
+// let c = 10;
+// let f = parseInt(a / b); //8
+// let d = parseInt(a % b); //75
+// let q = parseInt(d / c); //7
+// let g = parseInt(d % c); //5
+// console.log(g * b + q * c + f);
+
+//1-Boolean
+// let A = 2;
+// let aSonimusbat = A > 0;
+// console.log(aSonimusbat);
+
+//2-Boolean
+
+// let A = 7;
+
+// let toq = A % 2 === 0;
+// let juft = A % 2 !== 0;
+// console.log("Juft:", toq);
+// console.log("Toq:", juft);
+
+//3-boolen
+
+// let A = 7;
+
+// let toq = A % 2 === 0;
+// let juft = A % 2 !== 0;
+// console.log("Juft:", toq);
+// console.log("Toq:", juft);
+
+//4-boolen
+// let a = 4;
+// let b = 9;
+// let c = a > 2;
+// let d = b <= 3;
+// console.log(c, d);
+
+// 5-Boolean
+
+// let a = -2;
+// let b = -9;
+// let c = a >= 0;
+// let d = b < -2;
+// console.log(c, d);
+
+// 6-Boolean
+// let a = 3;
+// let a = 1;
+// let b = 4;
+// let c = -2;
+
+// if (a <= b <= c) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// console.log(d);
+
+// 7-Boolean
+// let a = 9;
+// let b = 4;
+// let c = 2;
+
+// if (a < b || b > c) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+//8
+// let a = 3;
+// let b = 6;
+// let toq = a % 2 === 0;
+// let juft = b % 2 !== 0;
+// console.log("Juft:", toq);
+// console.log("Toq:", juft);
+
+//if-6
+// let a = 51;
+// let b = 12;
+// if (a > b) {
+//   console.log("a katta");
+// } else {
+//   console.log("error");
+// }
+
+// //if-7
+// let a = 53;
+// let b = 12;
+// if (a > b || a >= b) {
+//   console.log("kichigi shu");
+// } else {
+//   console.log("kichigi yuq");
+// }
+
+//if 8
+// let a = 1;
+// let b = 6;
+// if (a < b) {
+//   console.log(a, b);
+// } else {
+//   console.log("xato");
+// }
+
+// for -6
+let a = 20000;
+
+for (let i = 1; i <= a; i++) {
+  let b = 1.1;
+  console.log();
+}
